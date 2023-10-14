4b8060c7b353bda1c9319af5ba0b6c02cf001dfc
stirelshka8 <stirelshka8@gmail.com>
2023-10-14 19:50:26 +0300

    FIX

--- back_rsync.sh
+++ back_rsync.sh
@@ -42,27 +42,30 @@ read -p "Желаете создать резервные копии Docker ко
 if [ "$backup_containers" = "yes" ]; then
     docker_containers=$(sudo docker ps -q)
     if [ -n "$docker_containers" ]; then
+        # Создаем временную директорию для временных файлов Docker контейнеров
+        docker_temp_dir="$backup_dir/docker_temp"
+        mkdir -p "$docker_temp_dir"
         for container_id in $docker_containers; do
             container_name=$(sudo docker inspect --format="{{.Name}}" $container_id)
             container_name=${container_name:1}
 
-            echo -e "\e[92mСоздание резервной копии Docker контейнера $container_name...\e[0m"
-            sudo docker export -o "$backup_dir/$container_name.tar" $container_id
-            echo -e "\e[93mРезервная копия Docker контейнера $container_name создана.\e[0m"
+            echo -e "\e[92mСоздание временной резервной копии Docker контейнера $container_name...\e[0m"
+            sudo docker export -o "$docker_temp_dir/$container_name.tar" $container_id
+            echo -e "\e[93mВременная резервная копия Docker контейнера $container_name создана.\e[0m"
         done
+
+        # Упаковываем временные резервные копии Docker контейнеров в zip архив
+        echo -e "\e[92mУпаковка временных резервных копий Docker контейнеров в zip архив...\e[0m"
+        sudo zip -rj "$backup_dir/docker_containers.zip" "$docker_temp_dir"
+        echo -e "\e[93mРезервные копии Docker контейнеров упакованы в '$backup_dir/docker_containers.zip'.\e[0m"
+
+        # Удаляем временные файлы Docker контейнеров
+        echo -e "\e[92mУдаление временных файлов Docker контейнеров...\e[0m"
+        sudo rm -r "$docker_temp_dir"
+        echo -e "\e[93mВременные файлы Docker контейнеров удалены.\e[0m"
     else
         echo -e "\e[91mНет активных Docker контейнеров для резервирования.\e[0m"
     fi
-fi
-
-# Упаковываем резервные копии Docker контейнеров в zip архив и удаляем tar-архивы
-if [ -n "$docker_containers" ] && [ "$backup_containers" = "yes" ]; then
-    echo -e "\e[92mУпаковка резервных копий Docker контейнеров в zip архив...\e[0m"
-    sudo zip -rj "$backup_dir/docker_containers.zip" "$backup_dir"
-    echo -e "\e[93mРезервные копии Docker контейнеров упакованы в '$backup_dir/docker_containers.zip'.\e[0m"
-    echo -e "\e[92mУдаление tar-архивов...\e[0m"
-    sudo rm "$backup_dir"/*.tar
-    echo -e "\e[93mTar-архивы удалены.\e[0m"
 fi
 
 # Проверка на отсутствие каких-либо резервных копий
